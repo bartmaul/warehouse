fec70053272bfea5caeb3a9e079534dc17d330e1
bartmaul <pierre.rinfray@gmail.com>
2016-05-03 14:42:28 +0900

    Initial commit

---
+++ app/controllers/Products.java
@@ -0,0 +1,34 @@
+package controllers;
+
+// import play *
+import play.mvc.*;
+import views.html.*;
+
+// import Product model
+import models.Product;
+
+public class Products extends Controller {
+
+
+	public Result showBlank() {
+		return TODO;
+	}
+
+	public Result show(Long ean) {
+		return TODO;
+	}
+
+	public Result save() {
+
+		//Product product = Form.form(Product.class).bindFromRequest().get();
+		return TODO;
+		
+	}
+	
+	public Result list() {
+		return TODO;
+		}
+
+
+
+}
